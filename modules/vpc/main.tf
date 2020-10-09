edfee7da23b60a2760ae4e1682bd0c4468395ad5
Henryk Konsek <hekonsek@gmail.com>
2020-10-09 08:35:12 +0200

    Fixed deprecated interpolation.

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -4,7 +4,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.57.0"
 
-  name                 = "${var.name}"
+  name                 = var.name
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
